a31d1755abcf46d61a223996ef26c23c72e255f7
vinodhadar <vinod.adari121@gmail.com>
2025-09-26 13:52:11 +0530

    [200~08-special-vars.sh

--- ls05.sh
+++ ls05.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo enter the PAN number
-read -s $PAN_number
+read -s Pan_number
 echo "Pan entered is: $PAN_number"
+# we shousld not put $ before variable name while reading input
+# -s is for silent mode, it will not show what we are typing
