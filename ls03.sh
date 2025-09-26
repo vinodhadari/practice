357d2f193ddaaab101d4e2a1a6c4a5f62f7e9d74
vinodhadar <vinod.adari121@gmail.com>
2025-09-26 12:53:30 +0530

    [200~08-special-vars.sh

---
+++ ls03.sh
@@ -0,0 +1,10 @@
+
+
+# No space before or after equal
+PERSON1="Sachin"
+PERSON2="Rahul"
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning Shell scripting $PERSON1"
