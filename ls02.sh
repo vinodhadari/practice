b755305dc37a1415d3e35f18a1be7b4f08280e6d
vinodhadar <vinod.adari121@gmail.com>
2025-09-26 12:43:27 +0530

    [200~08-special-vars.sh

---
+++ ls02.sh
@@ -0,0 +1,5 @@
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "Suresh:: I am learning Shell scripting Ramesh"
