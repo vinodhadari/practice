e1e757e4ae6901c9ff27161525b7352c38c98af4
vinodhadar <vinod.adari121@gmail.com>
2025-09-26 13:09:39 +0530

    [200~08-special-vars.sh

--- ls03.sh
+++ ls03.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# No space before or after equal
-PERSON1="Sachin"
-PERSON2="Rahul"
+PERSON1=ramesh
+PERSON2=suresh
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
