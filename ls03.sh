dbcb7941e6abe218273b58195c0564c3e7d3a702
vinodhadar <vinod.adari121@gmail.com>
2025-09-26 13:13:54 +0530

    [200~08-special-vars.sh

--- ls03.sh
+++ ls03.sh
@@ -3,7 +3,7 @@
 PERSON1=ramesh
 PERSON2=suresh
 
-echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: What are you learning recently?"
 echo "$PERSON2:: I am learning Shell scripting $PERSON1"
