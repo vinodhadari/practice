5988a4190000ddd193a4d4e7fcc7793dbd3800ec
vinodhadar <vinod.adari121@gmail.com>
2025-09-26 15:36:19 +0530

    [200~08-special-vars.sh

--- ls06.sh
+++ ls06.sh
@@ -6,8 +6,3 @@ export INSTRUCTOR="Jane Doe"
 export PLATFORM="AWS EC2"
 export OS="RHEL"
 
-# Print the values of the environment variables
-echo "Course name is: $COURSE"
-echo "Instructor is: $INSTRUCTOR"
-echo "Platform is: $PLATFORM"
-echo "Operating System is: $OS"
