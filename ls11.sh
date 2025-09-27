eaf4558b57ba69da197f8b88a12ad0a7bd51de11
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 16:51:30 +0530

    ls11.sh

--- ls11.sh
+++ ls11.sh
@@ -3,7 +3,7 @@
 echo "please enter the numbefr below"
 read NUMBER
 
-if [ ($NUMBER % 2) -eq 0 ]; then 
+if [ (($NUMBER % 2)) -eq 0 ]; then 
   echo "given number $NUMBER is eveb "
 else 
   echo "given number $NUMBER IS odd "
