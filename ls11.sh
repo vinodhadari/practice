370a9f3d2ddd9f42ab62655d80e4aee962ea5a50
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 17:21:59 +0530

    ls11.sh

--- ls11.sh
+++ ls11.sh
@@ -1,10 +1,17 @@
-#!/bin/bash/
+#!/bin/bash
 
-echo "please enter the numbefr below"
+echo "Please enter the number:"
 read NUMBER
 
-if [ (($NUMBER % 2) == 0 ) ]; then 
-  echo "given number $NUMBER is eveb "
-else 
-  echo "given number $NUMBER IS odd "
+if [ $(($NUMBER % 2)) -eq 0 ]; then
+    echo "Given number $NUMBER is EVEN"
+else
+    echo "Given number $NUMBER is ODD"
 fi
+
+# In short → [ ] can compare numbers, but it cannot do arithmetic calculations
+# we use $(( )) ,To perform arithmetic calculations
+# % is modulus operator which gives remainder after division
+# "if [ 0 -eq 0 ] ; then   # for even
+#  "if [ 1 -eq 0 ] ; then   # for odd
+#  Now [ ] can understand it, because it’s just comparing numbers with -eq.
