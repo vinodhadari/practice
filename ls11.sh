bffc736272b8d3afb40d07cbe1071f536ad1106f
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 17:32:30 +0530

    ls11.sh

--- ls11.sh
+++ ls11.sh
@@ -4,6 +4,11 @@ echo "Please enter the number:"
 read NUMBER
 
 if [ $(($NUMBER % 2)) -eq 0 ]; then #if (( NUMBER % 2 == 0 )); then is also correct
+#$(($NUMBER % 2)) → arithmetic expansion.
+# Bash computes % first.
+#[ ] (the test command) now just compares two numbers (1 -eq 0).
+#(( ... )) is arithmetic evaluation, not string test. 
+# so [ $(( ... )) -eq 0 ] is like a universal commnd
     echo "Given number $NUMBER is EVEN"
 else
     echo "Given number $NUMBER is ODD"
