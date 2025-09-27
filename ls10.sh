b0c9b1131a51b803b33b01be6ac6d23df7089b42
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 13:50:56 +0530

    ls10.sh

--- ls10.sh
+++ ls10.sh
@@ -1,11 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
 number=$1
 
-if [$number -lt 10 ]; then
+if [ $number -lt 10 ]; then
     echo "Given $number is less than 10"
-elif [$number -eq 10 ];then
+elif [ $number -eq 10 ];then
     echo "Given $number is equal to 10"
 else
-        echo"Given $number is greater than 10"
+        echo "Given $number is greater than 10"
 fi
