c478229fb1dc3bd94f3e20665b0cc0b1300e38b0
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 18:42:30 +0530

    ls12.sh

--- ls12.sh
+++ ls12.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-echo "Please enter the number below"
-read NUMBER
+# $? -> previous command exit status
+# 0 -> SUCCESS
+# 1-127 -> FAILURE
+# root user id =0
+# user id -> (id -u)
 
-if (( NUMBER % 2 == 0 )); then
-  echo "Given number $NUMBER is even"
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MySQL is failure"
+    exit 1
 else
-  echo "Given number $NUMBER is odd"
+    echo "Installing MySQL is SUCCESS"
 fi
