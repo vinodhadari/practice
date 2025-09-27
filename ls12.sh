82d10e7e0b730ad90cad85ad48b8c49f8c3f6cb9
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 22:20:48 +0530

    ls12.sh

--- ls12.sh
+++ ls12.sh
@@ -10,12 +10,13 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0
-fi
+    exit 1 # failure is other than 0,we are forcing thinking the above case might fail
+    #the main disadvantage of shell script is even if we get error it will continue that's why we are forcing to exit using exit 1 cmd
+    # uf exit 0 given it will continue and procede
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]; then # $? -> Exit status of the last command that was run
     echo "ERROR:: Installing MySQL is failure"
     exit 1
 else
