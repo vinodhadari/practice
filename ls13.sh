5a89dba43760cfa5147c274d0b69ba0e38c1d471
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 23:11:26 +0530

    ls13.sh

--- ls13.sh
+++ ls13.sh
@@ -10,23 +10,25 @@ fi
 dnf install mysql -y
 if [ $? -ne 0 ]; then 
     echo "error ::mysql installation failed "
+#else 
+    #echo " mysql is 100 correct " #we should place outside if exit 1
+    exit 1
 else 
     echo " mysql is 100 correct "
-    exit 1
 fi
 
 dnf install nginx -y
 if [ $? -ne 0 ]; then 
     echo "error ::ngnix installation failed "
+    exit 1
 else 
     echo " ngnix is 100 correct "
-    exit 1
 fi
 
 dnf install python3 -y
 if [ $? -ne 0 ]; then 
     echo "error ::python3 installation failed "
+    exit 1
 else 
     echo " python3 is 100 correct "
-    exit 1
 fi
