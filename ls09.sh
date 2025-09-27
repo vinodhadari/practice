6b395ead88818352bd3704dee0a6e3aee432cc99
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 13:13:58 +0530

    ls09.sh

--- ls09.sh
+++ ls09.sh
@@ -10,8 +10,12 @@ SUM=$(($NUMBER1+$NUMBER2+$NAME))
 echo "SUM is: ${SUM}"
 
 # Size = 4, Max index = 3
+#given below is the example of array
+#{LEADERS[@]} represent all
 LEADERS=("Modi" "Putin" "Trudo" "Trump")
+#positioning index start from 0 1 2 3 etc
 
 echo "All leaders: ${LEADERS[@]}"
 echo "First Leader: ${LEADERS[0]}"
 echo "First Leader: ${LEADERS[10]}"
+#dicpite 10 is not index it wont show error since it isnot programming it leave vecant
