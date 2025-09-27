40467830b09b3286ac55ddaa2f5a015304a20e03
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 10:09:30 +0530

    ls08.sh

--- ls08.sh
+++ ls08.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
+sleep 50 &
+echo "PID of the last command in background is: $!"
