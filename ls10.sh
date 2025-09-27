1ee8b254eb31b3e9460740829a6739c301d98ab2
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 16:09:25 +0530

    ls10.sh

--- ls10.sh
+++ ls10.sh
@@ -9,3 +9,10 @@ elif [ $number -eq 10 ];then
 else
         echo "Given $number is greater than 10"
 fi
+#ls10.sh: line 5: [: -lt: unary operator expected
+#ls10.sh: line 7: [: -eq: unary operator expected
+#Given  is greater than 10 it print this because the 2 above commands didn't execute
+
+# -gt
+# -eq
+# -ne
