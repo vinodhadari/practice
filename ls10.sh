423b07ba563d1c4bd1a004ab0fb86b24f4631990
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 13:44:44 +0530

    ls10.sh

--- ls10.sh
+++ ls10.sh
@@ -2,9 +2,9 @@
 
 number=$1
 
-if [number -lt 10 ]; then
+if [$number -lt 10 ]; then
     echo "print $number is less than 10"
-elseif [number -eq 10 ];then
+elseif [$number -eq 10 ]; then
     echo"$number is equal to 10"
 else
         echo"$number is greater than 10"
