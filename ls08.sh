bb4989c5c29ce0ce5fd5f2cc296b59333998b26a
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-27 10:12:20 +0530

    ls08.sh

--- ls08.sh
+++ ls08.sh
@@ -8,5 +8,5 @@ echo "Current directory: $PWD"
 echo "Who is running this: $USER"
 echo "Home directory of user: $HOME"
 echo "PID of this script: $$"
-sleep 50 &
+sleep 50 
 echo "PID of the last command in background is: $!"
