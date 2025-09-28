4f6fe0626595e82cff45f264251db7ecb9886ff5
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-28 21:57:03 +0530

    ls14.sh

--- ls14.sh
+++ ls14.sh
@@ -5,7 +5,7 @@
 # It tells the terminal:
   #“The following characters are a control sequence, not regular text.”
 R="\e[31m"
-G=\e[32m
+G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 # Without quotes, Bash tries to interpret \e[32m immediately, not as a string. That breaks your color variable.
