1c21d8b7861adea0cf8cb5f5b149499b658304ea
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-28 14:44:11 +0530

    ls13.sh

---
+++ ls13.sh
@@ -0,0 +1,25 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR ::change to root user"
+    exit 1
+fi
+# we give $? as input argument to $1 and ,installation software as input arguement for $2
+VALIDATE() {
+        if [ $1 -ne 0 ]; then # functions receive inputs through args just like shell script args
+            echo "error ::$2 installation failed "
+            exit 1
+        else 
+            echo " $2 is 100 correct "
+        fi
+}
+dnf install mysql -y
+VALIDATE() $? "mysql"
+
+dnf install nginx -y
+VALIDATE() $? "nginx"
+
+dnf install python3 -y
+VALIDATE() $? "python3"
