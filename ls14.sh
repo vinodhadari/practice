1ae120848ba286efe2f251ee7f19df26d3657f16
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-28 17:45:37 +0530

    ls14.sh

--- ls14.sh
+++ ls14.sh
@@ -1,28 +1,9 @@
-#!/bin/bash/
+#!/bin/bash
 
-USERID=$(id -u)
-# dnf remove mysql mysql-server
-if [ $USERID -ne 0 ]; then
-    echo "ERROR ::change to root user"
-    exit 1
-fi
-# we give $? as input argument to $1 and ,installation software as input arguement for $2
-VALIDATE() {
-        if [ $1 -ne 0 ]; then # functions receive inputs through args just like shell script args
-            echo "error ::$2 installation failed "
-            exit 1
-        else 
-            echo " $2 is 100 correct "
-        fi
-}
-dnf install mysql -y
-mysql=$2
-VALIDATE $? 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-dnf install nginx -y
-nginx=$2
-VALIDATE $? 
-
-dnf install python3 -y
-python3=$2
-VALIDATE $? 
+echo -e "$G Hello World $N"
+echo "Check this color"
