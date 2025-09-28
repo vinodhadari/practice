8fd8968054b5ef422bae26283cfff03e7094de0d
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-28 18:05:16 +0530

    ls14.sh

--- ls14.sh
+++ ls14.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# \e -> enable
+# -e in echo -e is required to interpret \e sequences
+# \033 can be used instead of \e
+# It tells the terminal:
+  #“The following characters are a control sequence, not regular text.”
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
