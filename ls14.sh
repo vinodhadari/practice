8093ebd31efd2acac14d9920e007c012cbc1fadf
vinodhadari <48354574+vinodhadari@users.noreply.github.com>
2025-09-28 21:55:18 +0530

    ls14.sh

--- ls14.sh
+++ ls14.sh
@@ -8,6 +8,9 @@ R="\e[31m"
 G=\e[32m
 Y="\e[33m"
 N="\e[0m"
-
+# Without quotes, Bash tries to interpret \e[32m immediately, not as a string. That breaks your color variable.
+# Quoting ensures \e is stored as text inside the variable,Then echo -e interprets it as an escape sequence.
+# So now, "Hello World" should show up in green, and "Check this color" in normal text
+# Always quote escape sequences when assigning them to variables in Bash ("\e[31m", not \e[31m).
 echo -e "$G Hello World $N"
 echo "Check this color"
